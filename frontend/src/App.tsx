faf0abc4e21ee3a08cea1912128d2f598e1fc392
Joshua Reno <jreno@cpp.edu>
2024-09-05 14:27:27 -0700

    readd frontend code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,38 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import About from './About'; // Import the About component
 import './About.css';
+import { Teammate } from './schema';
+import { getData } from './remote';
 
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0); // Specify type for state
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState<Teammate[] | null>(null);
+  const handleDBClick = async () => {
+    setLoading(false);
+    try {
+      const res = await getData();
+      setData(res);
+    } catch (e){
+      console.log(e)
+      setError(true);
+    }
+    setLoading(false);
+  }
 
+  const dbResult = () => {
+    if (error) {
+      return <p>Failed to connect to db:</p>
+    }
+    if (loading) {
+      return <p>Loading</p>
+    }
+    let i = 0;
+    return <div>
+      {data?.map(tm => <p key={i++}>{tm.name} {tm.fruit}</p>)}
+    </div>
+  };
   return (
     <Router>
       <div>
@@ -47,6 +75,12 @@ const App: React.FC = () => {
                 <p className="read-the-docs">
                   Click on the Vite and React logos to learn more
                 </p>
+                <div>
+                  <button onClick={handleDBClick}>
+                    Click me to load the table
+                  </button>
+                  {dbResult()}
+                </div>
               </>
             }
           />
