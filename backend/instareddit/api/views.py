da50b77965cc7ac07e713987fd85383b7ccd2691
Tony <tony@b01-aruba-authenticated-10-110-228-76.wlan.cpp.edu>
2024-09-11 12:15:01 -0700

    updated login view

--- backend/instareddit/api/views.py
+++ backend/instareddit/api/views.py
@@ -21,7 +21,7 @@ class RegisterUserView(views.APIView):
         # get data from request, validate form and return error status if data incomplete
         form = forms.UserRegisterForm(request.data)
         if not form.is_valid(): 
-            return Response("Incomplete or incorrect user data", status=status.HTTP_400_BAD_REQUEST)
+              return Response("Incomplete or incorrect user data", status=status.HTTP_400_BAD_REQUEST)
         username = form.cleaned_data['username']
         email = form.cleaned_data['email']
         password = form.cleaned_data['password']
@@ -32,7 +32,10 @@ class RegisterUserView(views.APIView):
 
         #hash password
         pw_bytes = password.encode('utf-8') #convert to bytes
-        pw_hash = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
+        salt = bcrypt.gensalt(10)
+        pw_hash = bcrypt.hashpw(pw_bytes, salt)
+        pw_hash= str(pw_hash)
+        
 
         #store user in DB 
         #TODO match fields with final User model
@@ -49,4 +52,36 @@ class RegisterUserView(views.APIView):
 #TODO
 class LoginView(views.APIView):
     def post(self, request, *args, **kwargs):
-        return Response()
+        form = forms.UserLoginForm(request.data)
+        
+        if not form.is_valid(): 
+            return Response("Incomplete or incorrect user data", status=status.HTTP_400_BAD_REQUEST)
+        
+	 
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        pw_bytes = password.encode('utf-8')
+        
+        
+        
+        # Check if user exists already
+        if not models.User.objects.filter(username=username).exists():
+            return Response("User with this username does not exist", status=status.HTTP_400_BAD_REQUEST)
+        
+        user = models.User.objects.get(username=username)
+        hashpass = user.password_hash
+
+        #testpassword = 'secret'
+        #testpassword = testpassword.encode('utf-8')
+        #salt = bcrypt.gensalt(10)
+        #hashtest = bcrypt.hashpw(testpassword, salt)
+        #hashtest = str(hashtest)
+        
+        
+        if bcrypt.checkpw(pw_bytes, hashpass):
+            key = os.environ.get('TOKEN_KEY')
+            token = jwt.encode({'username': username}, key, algorithm='HS256')
+            response = {'username': username, 'token': token}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            return Response("Incorrect password", status=status.HTTP_401_UNAUTHORIZED)
