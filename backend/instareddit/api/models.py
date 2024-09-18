1cadd12b285c937781b893a12b44460a1a247f65
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-09-17 22:03:00 -0700

    add friendships to User model

--- backend/instareddit/api/models.py
+++ backend/instareddit/api/models.py
@@ -7,11 +7,11 @@ class Dummy(models.Model):
     class Meta:
         db_table = 'dummy'
 
-#TODO just for testing, wait till final model is done
 class User(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, default=None)
     password_hash = models.CharField(max_length=1000)
+    friends = models.ManyToManyField('self', db_table='friendship')
     class Meta:
         db_table = 'user'
         
