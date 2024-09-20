b80b169c2d244777f4f997ace8fb152f80ecbe90
Joshua Reno <jralphur@gmail.com>
2024-09-20 15:31:56 -0700

    remove building of frontend

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit
-npm --prefix frontend install 
-NODE_ENV="production" npm --prefix frontend run build
-cp -r frontend/dist backend/instareddit
+# npm --prefix frontend install 
+# NODE_ENV="production" npm --prefix frontend run build
+# cp -r frontend/dist backend/instareddit
 pip install -r backend/requirements.txt
 python3 backend/instareddit/manage.py collectstatic --no-input
 python3 backend/instareddit/manage.py migrate
