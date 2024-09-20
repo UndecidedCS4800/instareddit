27c6d859ebf24e6e6e433ce54f10f28ae3991311
Joshua Reno <jralphur@gmail.com>
2024-09-20 15:57:04 -0700

    remove unused

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter, } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import About from './About'; // Import the About component
 //import './About.css';
