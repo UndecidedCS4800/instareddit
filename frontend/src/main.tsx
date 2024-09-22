5e329885e1b9e8b64892979469dfe5d38e5ad494
Joshua Reno <jralphur@gmail.com>
2024-09-21 19:07:20 -0700

    use browserrouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './About.tsx'
+import Login from './Login.tsx'
 
+const router = createBrowserRouter([
+  { path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
