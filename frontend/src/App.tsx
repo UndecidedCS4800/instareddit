ba4245fd52e8ce55ec6a3d94e9c5e3008d138b99
Joshua Reno <jralphur@gmail.com>
2024-09-21 21:37:36 -0700

    login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,13 @@
 import {Outlet} from 'react-router-dom';
 import './App.css';
-//import './About.css';
 import NavBar from './NavBar';
-// import Login from './Login';
-// import Register from './Register';
-
 const App: React.FC = () => {
 
   return (
-      <div> 
-        {/* use navbar */}
-        <NavBar />
-      {/*
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-          </ul>
-        </nav>
-  */}
-
+    <>
+      <NavBar />
       <Outlet />
-      </div>
+    </>
   );
 }
 
