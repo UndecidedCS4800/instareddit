cf1b96162934c280d9839c93590918cd08f93675
Joshua Reno <jreno@cpp.edu>
2024-09-27 11:11:17 -0700

    frontend-browserouter

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -3,12 +3,16 @@ import {Link} from 'react-router-dom';
 import './NavBar.css';
 import { useAuth, useAuthDispatch } from "./components/auth";
 
+function Greeting({name, ...props}: React.HTMLProps<HTMLAnchorElement> & {name: string}) {
+    return <a {...props}>Hello, {name}</a>
+}
+
 const NavBar: React.FC = () => {
     const auth = useAuth()
     const authDispatch = useAuthDispatch()
     let loginLink =  (<Link to="/login">Login</Link>)
     if (auth) {
-        loginLink = <a onClick={() => authDispatch({type: "logout"})}>Hello, {auth.username}</a>
+        loginLink = <Greeting name={auth.username} onClick={() => authDispatch({type: "logout"})} />
     }
     return (
         <nav className="navbar">
