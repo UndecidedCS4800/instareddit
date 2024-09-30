e1da5e3b5cfcb91cab96c7b09b472aac70ee7771
Joshua Reno <jreno@cpp.edu>
2024-09-30 14:17:40 -0700

    add types for backend

--- frontend/src/remote.ts
+++ frontend/src/remote.ts
@@ -1,7 +1,7 @@
-import { JWTTokenResponse } from "./schema";
+import { Community, JWTTokenResponse, PaginationResponse, Post, ServerError } from "./schema";
 
 const URL = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_BACKEND_PORT}`;
-
+type ResponseOrError<T> = T | ServerError;
 export const getData = async <T>(relative_path: string): Promise<T[]> => {
     try {
         const response = await fetch(`${URL}/api/${relative_path}`, {
@@ -88,3 +88,45 @@ export const logoutUser = () => {
         console.error('Error logging out:', error);
     }
 };
+
+
+async function get<T>(relative_path: string): Promise<ResponseOrError<T>> {
+    try {
+        const req = await fetch(`${URL}${relative_path}`)
+        if (!req.ok) {
+            const json = req.json()
+            return json as unknown as ServerError
+        }
+        const json = req.json();
+        return json as unknown as T
+    } catch (e) {
+        if (e instanceof Error) {
+            console.log("Unhandled error:", e.name, e.message)
+            return {error: e.message}
+        }
+        console.log("Unknown exception thrown")
+        return {error: "Unknown exception thrown"}
+    }
+}
+
+export const getCommunities = async (query?: string): Promise<ResponseOrError<PaginationResponse<Community[]>>> => {
+    const query_string = query ? `?query={${query}}` : ""
+    return await get(`/api/communities${query_string}`)
+}
+
+export const getCommunityPosts = async (id: number): Promise<ResponseOrError<PaginationResponse<Post>>> => {
+    return await get(`/api/community/${id}`)
+}
+
+export const getCommunity = async (id: number): Promise<ResponseOrError<Community>> => {
+    return await get(`/api/community/${id}/about`)
+}
+
+export const getPostComments = async (communityid: number, postid: number) : Promise<ResponseOrError<Post[]>> => {
+    return await get(`/api/community/${communityid}/post/${postid}`)
+}
+
+export const getUserPosts = async (username: string): Promise<ResponseOrError<Post[]>> => {
+    return await get(`/api/${username}/posts`)
+}
+
--- frontend/src/schema.ts
+++ frontend/src/schema.ts
@@ -3,14 +3,14 @@ export interface Teammate {
     name: string,
 }
 
-export interface user {
+export interface User {
     id: number, // Primary key for User
     username: string,
     email: string,
     password_hash: string,
   }
 
-export interface userinfo {
+export interface UserInfo {
     user: number,
     first_name: string,
     last_name: string,
@@ -18,16 +18,17 @@ export interface userinfo {
     profile_picture: string,
   }
 
-export interface post {
+export interface Post {
   id: number, // Primary key for Post
   user: number, // Foreign key to the user
   text: string,
   image: string,
   datetime: string,
   community: number, // Foreign key to the community
+  comments?: Comment[]
 }
 
-export interface comment {
+export interface Comment {
   id: number, // primary key for Comment
   user: number, // reference to the user who made the comment
   post: number, // reference to the associated Post
@@ -35,7 +36,7 @@ export interface comment {
   datetime: string, // timestamp of the comment
 }
 
-export interface community {
+export interface Community {
   id: number, //primary key for Community
   name: string,
   description: string,
@@ -43,7 +44,8 @@ export interface community {
   owner: number,
 }
 
-export interface recentactivity {
+
+export interface RecentActivity {
   id: number, // Primary key for Recent Activity
   userid: number, // Foreign key for the User
   postid: number, //Foreign key for the Post
@@ -51,14 +53,14 @@ export interface recentactivity {
   datetime: string,
 }
 
-export interface like {
+export interface Like {
   id: number, //primary key for like
   user: number, //Foreign key for the User
   post: number, //Foreign key for the Post
   datetime: string,
 }
 
-export interface dislike {
+export interface Dislike {
   id: number, //primary key for like
   user: number, //Foreign key for the User
   post: number, //Foreign key for the Post
@@ -77,7 +79,19 @@ export interface CommunityMembers {
   userid: number,
 }
 
+
 export interface JWTTokenResponse {
   username: string,
   token: string,
+}
+
+export interface PaginationResponse<T> {
+  count: number,
+  next: string | null,
+  prev: string | null,
+  results: T[],
+}
+
+export interface ServerError {
+  error: string
 }
