84899add848f2925cad7a9eb8fa9d22940e1f502
Joshua Reno <jreno@cpp.edu>
2024-10-04 16:49:32 -0700

    added connection tests to chat

---
+++ chat/src/index.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import http from "http"
+import mariadb from "mariadb"
+import cors, { CorsOptions } from "cors"
+import { createClient as createRedisClient } from "redis"
+
+const corsOptions: CorsOptions = {
+    origin: ["localhost"]
+}
+
+// note: express might not be needed at all, but we use it for cors currently
+const exp = express()
+exp.use(cors(corsOptions))
+exp.get('/', (_req, res) => {
+    res.json({"message": "Hello, world!"})
+})
+
+// redis connection
+const redisClient = createRedisClient({
+    socket: {
+        host: "chatlog"
+    },
+    username: "instareddit",
+    password: "secret",
+    database: 0,
+})
+
+redisClient.on("connection", conn => console.log("Connected", conn))
+redisClient.on("error", error => console.error("redis client error", error))
+
+redisClient.connect().then(() => console.log("connected to redis client"))
+                     .catch(err => console.error("Failed to connect:", err))
+
+
+// mariadb connection
+const pool = mariadb.createPool({
+    host: "db",
+    user: "root",
+    password: "secret",
+    database: "dummy" 
+})
+
+pool.getConnection().then(_conn => console.log("Connected to MariaDB"))
+                    .catch(err => console.error("Failed to connect:", err))
+const server = http.createServer(exp)
+
+server.listen(process.env.PORT, () => {
+    console.log("Server started on", process.env.PORT)
+})
+
