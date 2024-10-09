178797442d9a09240ab5e714c08d2436649c3014
xvanthony <112587667+xvanthony@users.noreply.github.com>
2024-10-09 00:46:43 -0700

    adding meeting logs and fix stlying bug for community page

--- frontend/src/components/CommunityCard.tsx
+++ frontend/src/components/CommunityCard.tsx
@@ -20,13 +20,6 @@ export const CommunityCard = ({ community }: CommunityCardProps) => {
                 <div className="text-white text-xl font-bold font-sans">{name}</div>
             </div>
             {picture && <img className="h-[350px] w-full object-cover" src={picture} alt={name} />}
-            <div className="h-[55px] px-[12px] flex items-center gap-4">
-                <img className="w-[29px] h-[29px]" src={ThumbUp} alt="Thumb Up" />
-                <img className="w-[29px] h-[29px]" src={ThumbDown} alt="Thumb Down" />
-                <img className="w-[29px] h-[29px]" src={ToolTip} alt="Tooltip" />
-                <img className="w-[29px] h-[29px] ml-auto" src={Reply} alt="Reply" />
-            </div>
-            
             <p className="px-4 py-2 text-white flex">{description}</p>
         </Card>
     );
