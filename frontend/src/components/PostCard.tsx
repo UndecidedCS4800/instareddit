8da388f7c8118f3cfe0e8a3522b65b7e6e3389cc
Joshua Reno <jreno@cpp.edu>
2024-10-18 15:57:18 -0700

    profile link for postcard

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,20 +1,20 @@
 import { Post } from "../schema"
-import { Card } from "./Card"
+import ProfileLink from "./ProfileLink"
 
 type PostCardProps = {
     post: Post
     link?: boolean
 }
 
-const Elem = ({link, to, children}: { link?: boolean, to: string, children: React.ReactNode }) => {
-    return link ? <Card to={to}>{children}</Card> : <div>{children}</div>
+const Elem = ({link, username, children}: { link?: boolean, username: string, children: React.ReactNode }) => {
+    return link ? <ProfileLink user={username}>{children}</ProfileLink> : <div>{children}</div>
 }
 export const PostCard = ({post, link}: PostCardProps) => {
     // TODO?: truncate post preview text
     const { datetime, image,  username, text } = post
 
     return (
-        <Elem link={link} to={`posts/${post.id}`}>
+        <Elem link={link} username={username}>
             <div className="px-4 py-4 justify-start items-center gap-5 flex">
                 <div className="text-white text-xl font-bold font-sans">{username}</div>
                 {image && <img className="h-[350px] w-full object-cover"src={image} />}
