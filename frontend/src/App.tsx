19f49170b66aafe14f5d581f1132738d2ba1dcc3
Joshua Reno <jreno@cpp.edu>
2024-10-25 17:47:44 -0700

    move socket connection to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,23 @@ import {Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 import { CenterViewContainer } from './components/CenterViewContainer';
 import Pane from './components/Pane';
+import { useEffect } from 'react';
+import socket from './socket';
+import { useAuth } from './components/auth';
+
 
 
 const App: React.FC = () => {
-
+  const auth = useAuth()
+  useEffect(() => {
+    const connect = () => {
+      if (auth) {
+        socket.auth = { token: auth.token };
+        socket.connect();
+      }
+    };
+    connect();
+  }, [auth]);
 
 
   return (
