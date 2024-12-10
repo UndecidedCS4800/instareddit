81c004ff367f15181224baf61faa6c16b0ae7b5d
Joshua Reno <jreno@cpp.edu>
2024-12-10 15:43:00 -0800

    [skip render] checkout backend

--- backend/instareddit/api/urls.py
+++ backend/instareddit/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .controllers import community_views, auth_views, post_views, profile_views, friendship_views, search_view
+from .controllers import community_views, auth_views, post_views, profile_views, friendship_views, notifications_views, search_view
 
 urlpatterns = [
     #authentication routes
@@ -30,6 +30,9 @@ urlpatterns = [
     #profiles
     path('profile/<str:username>' , profile_views.ProfileView.as_view()),
     path('profile/', profile_views.SelfProfileView.as_view()),
+    path('profile/notifications/likes',notifications_views.NotificationsGetLikesViews.as_view()),
+    path('profile/notifications/comments',notifications_views.NotificationsGetCommentsViews.as_view()),
+
 
     #friends
     path('friends/', friendship_views.FriendsIdsGetView.as_view()),
