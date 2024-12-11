46d0c4564be11adf8500b898438e92f1653a77e3
xvanthony <112587667+xvanthony@users.noreply.github.com>
2024-12-10 21:30:51 -0800

    fixed routing

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -11,33 +11,40 @@ type PostCardProps = {
     privileged: boolean
 }
 
-const Elem = ({link, to, children}: { link?: boolean, to: string, children: React.ReactNode }) => {
+const Elem = ({ link, to, children }: { link?: boolean, to: string, children: React.ReactNode }) => {
     return link ? <Card to={to}>{children}</Card> : <div>{children}</div>
 }
-export const PostCard = ({post, privileged = false, link}: PostCardProps) => {
-    // TODO?: truncate post preview text
-    const { datetime, image,  username, text } = post
+
+export const PostCard = ({ post, privileged = false, link }: PostCardProps) => {
+    const { datetime, image, username, text, community, id } = post
     const auth = useAuth()
     const validator = useRevalidator()
+
     const handleRemove = async (e: React.MouseEvent) => {
         e.preventDefault()
         if (auth) {
-            const resp = await removePost(auth.token, post.community, post.id)
+            const resp = await removePost(auth.token, community, id)
             if (!isError(resp)) {
                 validator.revalidate()
             } else {
                 console.error("server error", resp)
             }
-
         }
     }
 
+    // Determine the link to use: Community or User link
+    const linkTo = community
+        ? `/community/${community}/posts/${id}`
+        : `/user/${username}/posts/${id}`
+
     return (
-        <Elem link={link} to={`/community/${post.community}/posts/${post.id}`}>
+        <Elem link={link} to={linkTo}>
             {auth && privileged && <button onClick={handleRemove}>rm</button>}
             <div className="px-4 py-4 justify-start items-center gap-5 flex">
-                <ProfileLink user={username} className="font-bold text-[#e78fcb] hover:text-white text-xl">{username}</ProfileLink>
-                {image && <img className="h-[350px] w-full object-cover"src={image} />}
+                <ProfileLink user={username} className="font-bold text-[#e78fcb] hover:text-white text-xl">
+                    {username}
+                </ProfileLink>
+                {image && <img className="h-[350px] w-full object-cover" src={image} />}
                 <div className="ml-auto text-white">{datetime.split("T")[0]}</div>
             </div>
             <div className="px-4 py-2 text-white flex">{text}</div>
