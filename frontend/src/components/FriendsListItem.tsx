33ec0ef824c2f12979e3abfe44e5b89f3969f922
xvanthony <112587667+xvanthony@users.noreply.github.com>
2024-12-10 17:54:07 -0800

    more styling

--- frontend/src/components/FriendsListItem.tsx
+++ frontend/src/components/FriendsListItem.tsx
@@ -10,7 +10,7 @@ const FriendsListItem = ({ friend, cardHandler, removeHandler }: FriendsListItem
     return (
         <div
             onClick={() => cardHandler()}
-            className="flex items-center hover:shadow-xl hover:text-[#e78fcb] justify-between p-2 text-white text-xl font-bold font-sans rounded-lg cursor-pointer"
+            className="group flex items-center hover:shadow-xl hover:text-[#e78fcb] justify-between p-2 text-white text-xl font-bold font-sans rounded-lg cursor-pointer"
         >
             <span>{friend.username}</span>
             <button
@@ -18,7 +18,7 @@ const FriendsListItem = ({ friend, cardHandler, removeHandler }: FriendsListItem
                     e.stopPropagation();
                     removeHandler();
                 }}
-                className="ml-4 p-1 text-white rounded-full hover:bg-[#e78fcb] hover:text-white"
+                className="ml-4 p-1 text-white opacity-0 bg-transparent focus:outline-none group-hover:opacity-100 transition-opacity duration-200 hover:text-[#e78fcb]"
             >
                 X
             </button>
